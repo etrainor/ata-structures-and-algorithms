8569eed4bbc2812d9cc2557a1178082f4401c40b
Erin Trainor <trainor.erin@gmail.com>
2018-11-29 00:36:53 -0800

    finished question 6

--- code-challenges/301/regex/challenges-04.test.js
+++ code-challenges/301/regex/challenges-04.test.js
@@ -82,11 +82,6 @@ The expected output of "Hello, and have a wonderful day!" is ["and ", "have ", "
 const noPunctuation = str => {
   let regex = /[^\s\W][a-zA-Z]*\s/g;
   let result = str.match(regex);
-  // let newArray = [];
-  // for (var i = 0 ; i < result.lenth ; i++){
-  //   if(result[i] !== " ") {
-  //     newArray.push(result);
-  //   }
   return result;
 };
 
@@ -103,7 +98,9 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
 let hangman = (str) => {
-  // Solution code here...
+  let regex = /[aeiou]/gm;
+  let result = str.replace(regex, '_');
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -200,7 +197,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   let startString = 'This is a regex challenge. We are trying to create a hangman phrase where all of the vowels are missing!';
 
   test('It should remove the vowels from the hangman string and replace them with underscores', () => {
