ae722f7c7a78b8b69e810fe487997e88c5770b35
Erin Trainor <trainor.erin@gmail.com>
2018-11-28 19:27:27 -0800

    stuck on 4

--- code-challenges/301/regex/challenges-04.test.js
+++ code-challenges/301/regex/challenges-04.test.js
@@ -62,7 +62,9 @@ Do not use the vertical bar (pipe) in your pattern.
 ------------------------------------------------------------------------------------------------ */
 
 const matchMonth = (input) => {
-  // Solution code here...
+  // oct case insensitive
+  let regex = /[oct]\w+/gi;
+  return regex.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -158,7 +160,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should match any of the acceptable inputs', () => {
     expect(matchMonth('Oct')).toBeTruthy();
     expect(matchMonth('oct')).toBeTruthy();
