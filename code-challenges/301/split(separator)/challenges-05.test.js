de74b48473654775044b556ff88dce5195aed593
Erin Trainor <trainor.erin@gmail.com>
2018-12-03 01:57:57 -0800

    finished question 6

--- code-challenges/301/split(separator)/challenges-05.test.js
+++ code-challenges/301/split(separator)/challenges-05.test.js
@@ -137,7 +137,11 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const removeEvenValues = (arr) => {
-  // Solution code here...
+  for(let i = 0 ; i < arr.length ; i++) {
+    while(arr[i] % 2 === 0){
+      arr.splice(i,1);
+    }
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -248,7 +252,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);
