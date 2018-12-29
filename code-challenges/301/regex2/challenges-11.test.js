580d19c74f55b47b573eb03df20fe166d39fbf98
Erin Trainor <trainor.erin@gmail.com>
2018-12-28 23:20:31 -0800

    finished 3

--- code-challenges/301/regex2/challenges-11.test.js
+++ code-challenges/301/regex2/challenges-11.test.js
@@ -58,7 +58,9 @@ Return either true or false.
 ------------------------------------------------------------------------------------------------ */
 
 const validatePhoneNumber = (phoneNumber) => {
-  // Solution code here...
+  let regex = /^(\d{3}|[(]\d{3}[)])(\s|[-])?\d{3}(\s|[-])?\d{4}$/
+  let result = regex.test(phoneNumber);
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -127,7 +129,7 @@ describe('Testing challenge 2', () => {
   })
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should match the acceptable phone number formats', () => {
     expect(validatePhoneNumber('(555) 555-5555')).toBeTruthy();
     expect(validatePhoneNumber('555 555-5555')).toBeTruthy();
