189ed4464fa0183815d3f8c4748add8ab7263ec1
Erin Trainor <trainor.erin@gmail.com>
2019-03-24 22:15:48 -0700

    added error catching

--- code-challenges/401/linkedList/linked-list.js
+++ code-challenges/401/linkedList/linked-list.js
@@ -15,6 +15,7 @@ class Node {
 // In a linked list, the first node in the the head. Size designates the number of nodes in the list.
 
 class LinkedList {
+  
   constructor() {
     this.head = null;
     this.size = 0;
@@ -24,15 +25,23 @@ class LinkedList {
 
   // Insert a new node at the beginning of a linked list
   insert(valueToInsert) {
+    try {
     // Create a new node
-    let newNode = new Node(valueToInsert);
+      let newNode = new Node(valueToInsert);
     
-    // Give the new node the value of head. This moves the new node before the current location of the head.
-    newNode.next = this.head;
+      // Give the new node the value of head. This moves the new node before the current location of the head.
+      newNode.next = this.head;
     
-    // Tell the new node that it is now the head (beginning) of the linked list
-    this.head = newNode;
-  }
+      // Tell the new node that it is now the head (beginning) of the linked list
+      this.head = newNode;
+
+      // Catch the errors and return something useful
+    } catch (error) {
+      if(error.stack) {
+        console.error('There was an error inserting a a new node', error, error.stack);
+      } else return error;
+    }
+  } 
 
   //////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////
@@ -40,15 +49,24 @@ class LinkedList {
   // Check if a linked list contains a specific value
   includes(value) {
     
-    // Tell the function to start at the beginning(head) of the list.
-    let currentNode = this.head;
+    try {
+
+      // Tell the function to start at the beginning(head) of the list.
+      let currentNode = this.head;
     
-    // Return a boolean value depending on whether the value exists in the linked list.
-    while(currentNode) {
-      if(currentNode === value) return true;
-    } 
+      // Return a boolean value depending on whether the value exists in the linked list.
+      while(currentNode) {
+        if(currentNode === value) return true;
+      } 
     
-    return false;
+      return false;
+
+      // Catch the errors and return something useful
+    } catch (error) {
+      if(error.stack) {
+        console.error('There was an error inserting a a new node', error, error.stack);
+      } else return error;
+    }
   } 
   
   //////////////////////////////////////////////////////////
@@ -57,17 +75,25 @@ class LinkedList {
   // A method that takes in no arguments and returns a collection of all the current Node values in the list
   print() {
     
+    try {
     // Tell the function to start at the beginning(head) of the list and create a variable that can console log the elements in the list.
-    let currentNode = this.head;
-    let outputString = '';
+      let currentNode = this.head;
+      let outputString = '';
 
-    // Iterate through the nodes and add the values found to the outputString. Continue to reassign the next node as the current value.
-    while(currentNode) {
-      outputString += currentNode.element + ' ';
-      currentNode = currentNode.next;
-    }
+      // Iterate through the nodes and add the values found to the outputString. Continue to reassign the next node as the current value.
+      while(currentNode) {
+        outputString += currentNode.element + ' ';
+        currentNode = currentNode.next;
+      }
     
-    // Console log the elements in the linked list
-    console.log(outputString);
+      // Console log the elements in the linked list
+      console.log(outputString);
+
+      // Catch the errors and return something useful
+    } catch (error) {
+      if(error.stack) {
+        console.error('There was an error inserting a a new node', error, error.stack);
+      } else return error;
+    }
   }
-}
+} 
